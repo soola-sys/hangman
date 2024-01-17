f4dd4da9eba7228f78e08657283821dd34c1a955
soola-sys <agdarbekovsultan@gmail.com>
2024-01-17 08:08:11 +0600

    render: hangman keyboard

---
+++ src/js/questions.js
@@ -0,0 +1,34 @@
+let questions = [{
+  id: 1,
+  hint: 'The Holiday',
+  answer: 'Halloween'
+}, {
+  id: 2,
+  hint: 'What you might get on Holiday',
+  answer: 'scared'
+}, {
+  id: 3,
+  hint: 'The Treat',
+  answer: 'candy'
+}, {
+  id: 4,
+  hint: 'What a with rides',
+  answer: 'Broomstick'
+}, {
+  id: 5,
+  hint: 'Witch',
+  answer: 'Casts a spell'
+}, {
+  id: 6,
+  hint: 'Might haunt you',
+  answer: 'Ghost'
+}, {
+  id: 7,
+  hint: 'Makes a good pie',
+  answer: 'pumpkin'
+}, {
+  id: 8,
+  hint: 'Find in Graveyard',
+  answer: 'skeleton'
+}];
+export { questions };
